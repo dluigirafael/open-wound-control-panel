935825a72dea09e4358d522eec2b009c0bc29e0a
Rafael Duma <--global>
2021-06-21 12:43:32 +0300

    move

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+	</h1>
+
+	<h2>
+		try editing <strong class="mx-5">src/routes/index.svelte</strong>
+	</h2>
+</section>
+
+<style>
+</style>
---
+++ src/routes/todos/index.js
@@ -0,0 +1,18 @@
+export async function get(request) {
+	return {
+		status: 200,
+		body: {
+			todo: 'todo'
+		}
+	};
+}
+export async function post(request) {
+	return {};
+}
+
+export async function put(request) {
+	return {};
+}
+export async function del(request) {
+	return {};
+}
