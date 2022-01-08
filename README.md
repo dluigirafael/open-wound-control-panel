cc2e95c25ac3b346e9544490946e4460c50a0166
Rafael Duma <dluigirafael12@yahoo.com>
2022-01-08 16:04:39 +0200

    update dep

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # open-wound-control-panel
 
-Bleeding edge hosting control panel built with Svelte and Deno
+Bleeding edge hosting control panel built with SvelteKit and Deno
 
 ## todos
 
--- client/src/lib/Header/headerButton.svelte
+++ client/src/lib/Header/headerButton.svelte
@@ -1,24 +1,24 @@
 <script>
-  import { page } from "$app/stores";
-  export let icon;
-  export let route;
+	import { page } from "$app/stores";
+	export let icon;
+	export let route;
 </script>
 
 <li class="mr-1 my-2 md:my-0">
-  <a
-    sveltekit:prefetch
-    href={route}
-    class={`flex justify-center items-center px-2 py-1 md:py-3 align-middle
+	<a
+		sveltekit:prefetch
+		href={route}
+		class={`flex justify-center items-center px-2 py-1 md:py-3 align-middle
     text-blue-400 no-underline hover:text-gray-100 border-b-2 border-blue-400
-    hover:border-blue-400 ${$page.path === route ? "active" : ""}`}
-  >
-    <span class="material-icons-round text-xl no-underline mr-2">{icon}</span>
-    <span class="pb-1 md:pb-0 text-sm"><slot /></span>
-  </a>
+    hover:border-blue-400 ${$page.url.pathname === route ? "active" : ""}`}
+	>
+		<span class="material-icons-round text-xl no-underline mr-2">{icon}</span>
+		<span class="pb-1 md:pb-0 text-sm"><slot /></span>
+	</a>
 </li>
 
 <style lang="scss">
-  .active {
-    border: 2px solid red;
-  }
+	.active {
+		border: 2px solid red;
+	}
 </style>
