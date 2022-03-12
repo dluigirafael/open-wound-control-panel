bba4a1f53a8baa22164fa786a4eefc66103db416
Rafael Duma <dluigirafael12@yahoo.com>
2022-03-12 15:15:39 +0200

    fix build

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -5,12 +5,16 @@ import path from "path";
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-
+		prerender: {
+			default: true,
+		},
 		adapter: adapter({
 			pages: "build",
 			assets: "build",
 			fallback: null,
+			precompress: false,
 		}),
+
 		vite: {
 			resolve: {
 				alias: {
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 import {
-  Application,
-  Router,
-  send,
-  config,
-  Session,
-  DashportOak,
-  LocalStrategy,
-  connect,
+	Application,
+	Router,
+	send,
+	config,
+	Session,
+	DashportOak,
+	LocalStrategy,
+	connect,
 } from "./dependencies/deps.js";
 // import { database } from "./database/db.js";
 import routes from "./routes/routes.js";
 
 let env = config();
 
-const redis = await connect({
-  hostname: env["REDIS_HOST"],
-  port: env["REDIS_PORT"],
-});
+// const redis = await connect({
+//   hostname: env["REDIS_HOST"],
+//   port: env["REDIS_PORT"],
+// });
 
-const ok = await redis.set("key", { nene: "tanti" });
-const fuga = await redis.get("key");
-console.log(fuga);
+// const ok = await redis.set("key", { nene: "tanti" });
+// const fuga = await redis.get("key");
+// console.log(fuga);
 
 const router = new Router();
 
@@ -34,14 +34,18 @@ app.use(routes.uname.prefix("/api").routes());
 app.use(routes.uname.allowedMethods());
 
 app.addEventListener("error", (evt) => {
-  console.log(evt.error);
+	console.log(evt.error);
 });
 
 app.use(async (context) => {
-  await send(context, context.request.url.pathname, {
-    root: `${Deno.cwd()}/client/build`,
-    index: "index.html",
-  });
+	await send(context, context.request.url.pathname, {
+		root: `${Deno.cwd()}/client/build`,
+		index: "index.html",
+	});
+});
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+	console.log(`Listening on: ${secure ? "https://" : "http://"}${hostname ?? "localhost"}:${port}`);
 });
 
 await app.listen({ port: 6900 });
